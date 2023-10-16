578a08e686a3ab9c02e414e82a4161b6fba6f701
Solomon Alan-Dei <solomon.alan-dei@kibo.school>
2023-10-16 14:10:42 +0200

    Add template for fizzbuzz

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def welcome_message():
     return "Hello world"
+
+@app.route("/fizzbuzz")
+def fizzbuzz():
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
